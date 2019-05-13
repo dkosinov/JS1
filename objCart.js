c30589b8f29626ceff262dce19ced141fb0d0f04
dkosinov <dkosinov@mail.ru>
2019-05-13 15:37:28 +0300

    Урок 4. Домашнее задание на проверку

---
+++ objCart.js
@@ -0,0 +1,54 @@
+var objCart = {
+    productList : [
+        {
+            product_1 : {
+                productId : '001',
+                quantity : 1,
+                getPrice : function () {
+                    var price;
+                    //запрос в базу данных цены для this.productId
+                    return price;
+                },
+                getSubTotal : function () {
+                    return this.getPrice() * this.quantity;
+                },
+            },
+        },
+    ],
+    getProductListForCartPage : function () {
+        var productListForCartPage = [];
+        //создаём массив объёктов по шаблону выдачи в страницу карзины
+        return productListForCartPage;
+    },
+    productAdd : function (pID) {
+        //
+    },
+    productDelete : function (pID) {
+        //
+    },
+    viewProductInSinglePage : function (pID) {
+        //
+    },
+    getTotal : function () {
+        var total;
+        return total;
+    },
+    customer : {},
+    setCustomer : function () {
+        //
+    },
+    shippingAdress : {},
+    getSippingAdress : function () {
+        //
+    },
+    couponDiscount : {},
+    getCouponDiscount : function () {
+        //
+    },
+    getGrandTotal : function () {
+        //
+    },
+    continueShoppingAddCheckOut : function () {
+        //
+    }
+},
