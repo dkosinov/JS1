6143e8ade3f7e04dfacd6271fdbf6b629d79b0fd
dkosinov <dkosinov@mail.ru>
2019-05-16 17:21:55 +0300

    Revert "Merge pull request #3 from dkosinov/js1-lesson-5"
    This reverts commit 5e016831425dafb585a2b14032471202a8448265, reversing
    changes made to 50fa38cf2f248754ea2f8134efe9f365e4c8e5cb.

--- objCart.js
+++ objCart.js
@@ -1,75 +1,36 @@
-var cartProductPrototype = {
-    getPrice : function () {
-        //здесь должен быть запрос в базу данных цены для this.productId
-        for (var i = 0; i < productsArr.length; i++) {
-            if (productsArr[i].id === this.productId)
-                return productsArr[i].price;
-        }
-    },
-    getSubTotal : function () {
-        //вернём стоимость всех единиц данного товара
-        return this.getPrice() * this.quantity;
-    },
-};
-
-
 var objCart = {
-    //создадим глобальный объект корзины покупок
-
-    productList : [], //корзина пуста
-
-    getTotalSumm : function () {
-        var totalSumm = 0;
-        for (var i = 0; i < this.productList.length; i++) {
-            totalSumm += this.productList[i].getSubTotal();
-        }
-        return totalSumm;
-    },
-    getTotalQuantity : function () {
-        var totalQuantity = 0;
-        for (var i = 0; i < this.productList.length; i++) {
-            totalQuantity += this.productList[i].quantity;
-        }
-        return totalQuantity;
-    },
-
-    getCartInfoPage : function () {
-        var $container = document.getElementById('container');
-        console.log($container.tagName);
-        var $cart_info = document.createElement('p');
-        $container.appendChild($cart_info);
-        if (this.productList.length > 0) {
-            $cart_info.textContent = "В корзине: " + this.getTotalQuantity() +
-                " товаров на сумму " + this.getTotalSumm().toFixed(2) + " рублей.";
-        } else {
-            $cart_info.textContent = 'Корзина пуста.';
-        }
-    },
-
+    productList : [
+            {
+                productId : '001',
+                quantity : 1,
+                getPrice : function () {
+                    var price;
+                    //запрос в базу данных цены для this.productId
+                    return price;
+                },
+                getSubTotal : function () {
+                    return this.getPrice() * this.quantity;
+                },
+            },
+    ],
     getProductListForCartPage : function () {
         var productListForCartPage = [];
-
         //создаём массив объёктов по шаблону выдачи в страницу карзины
         return productListForCartPage;
     },
-    addProductToCart : function (_id, _quantity) {
-        //добавим выбранный товар в корзину
-        var newProduct = Object.create(cartProductPrototype);
-        for (var i = 0; i < productsArr.length; i++) {
-            if (productsArr[i].id === _id) {
-                newProduct.productId = _id;
-                newProduct.quantity = _quantity;
-                this.productList.push(newProduct);
-            }
-        }
+    productAdd : function (pID) {
+        //
     },
-    removeProductFromCart : function (_id) {
+    productDelete : function (pID) {
         //
     },
     viewProductInSinglePage : function (pID) {
         //
     },
-
+    getTotal : function () {
+        var total;
+        return total;
+    },
     customer : {},
     setCustomer : function () {
         //
@@ -78,14 +39,14 @@ var objCart = {
     getSippingAdress : function () {
         //
     },
+    couponDiscount : {},
     getCouponDiscount : function () {
-        //Проверим наличие у покупателя скидок и вернём сумму скидки если есть
+        //
     },
     getGrandTotal : function () {
-        //вычитаем скидку
-        return this.getTotalSumm() - this.getCouponDiscount();
+        //
     },
-    continueShoppingCheckOut : function () {
+    continueShoppingAddCheckOut : function () {
         //
     }
 }
